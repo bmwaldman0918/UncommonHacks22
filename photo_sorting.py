501f720a2b9dd7d6b28671cbe6b02f3c9c3ae625
bmwaldman0918 <bwaldman@uchicago.edu>
2022-04-02 14:55:57 -0500

    basic outline of photo/photolib class

--- photo_sorting.py
+++ photo_sorting.py
@@ -1,1 +1,23 @@
+class PhotoLib:
+    def __init__(self, x: iter = None):
+        if x:
+            self.photos = list(x)
+        else:
+            self.photos = list()
+
+    ## returns a list of photos sorted by a given function
+    def sort(self, fun):
+        photos = list()
+
+        for i in self.photos:
+            photos.append(tuple(i, fun(i)))
+
+        photos.sort(key=lambda x: x[1])
+
+        return photos
+
+
 class Photo:
+    def __init__(self, pixel_array: list, name: str):
+        self.name = name
+        self.pixels = pixel_array
