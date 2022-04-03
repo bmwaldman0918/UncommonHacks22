3d99338f915c135620be10912f69b34673a78bce
Samuel Shen <samuelshen@Samuels-MacBook-Air-2.local>
2022-04-02 20:01:46 -0500

    jpeg to 4-vector array

---
+++ jpeg_to_array.py
@@ -0,0 +1,17 @@
+# Import the necessary libraries
+from PIL import Image
+from numpy import asarray
+
+# load the image and convert into
+# numpy array
+img = Image.open('Sample.png')
+
+# asarray() class is used to convert
+# PIL images into NumPy arrays
+numpydata = asarray(img)
+
+# <class 'numpy.ndarray'>
+print(type(numpydata))
+
+#  shape
+print(numpydata.shape)
