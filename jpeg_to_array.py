31002ee866d803a0aadc15bdc8ecfeb33c1269ce
Samuel Shen <samuelshen@Samuels-MacBook-Air-2.local>
2022-04-03 08:12:42 -0500

    jpeg_to_array

--- jpeg_to_array.py
+++ jpeg_to_array.py
@@ -1,14 +1,10 @@
-# Import the necessary libraries
 from PIL import Image
 from numpy import asarray
 
-# load the image and convert into
-# numpy array
-img = Image.open('Sample.png')
-
-# asarray() class is used to convert
-# PIL images into NumPy arrays
-numpydata = asarray(img)
+#takes name of jpeg and converts to 4-vector array (rgb values)
+def jpeg_to_4vector(name: str):
+    img = Image.open(name)
+    return asarray(img)
 
 
 
